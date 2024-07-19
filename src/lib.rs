053ebae8e0c95c035cda530754dd2cfbe08d292c
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-07-18 21:12:09 -0600

    Created conversion code
    Created the conversion code ready to be compiled into web assembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
+pub fn num_to_string(num: u8) -> String {
+    if num >= 10 {
+        return num.to_string();
+    }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    match num {
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        4 => "four".to_string(),
+        5 => "five".to_string(),
+        6 => "six".to_string(),
+        7 => "seven".to_string(),
+        8 => "eight".to_string(),
+        9 => "nine".to_string(),
+        _ => "NaN".to_string(),
+    }
 }
